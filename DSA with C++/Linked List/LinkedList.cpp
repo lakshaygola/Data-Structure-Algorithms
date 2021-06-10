c7f81e2776f2d2ed2b131fc51822de2db1e84607
Lakshay Gola <60484223+lakshaygola@users.noreply.github.com>
2021-06-10 16:22:36 +0530

    Add files via upload

---
+++ DSA with C++/Linked List/LinkedList.cpp
@@ -0,0 +1,29 @@
+/* Linked list and its basic operation */
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+// Class node to define the basic structure of the nodes
+class Node{
+private:
+	int data;
+	Node *next;
+
+public:
+	Node(int val){
+		data = val;
+		next = NULL;
+	}
+};
+
+// Function to inerst the node at the tail / end
+void insertToTail(Node* &head, int val){
+	Node* n = new Node(val);
+	
+}
+
+int main(){
+
+	return 0;
+}
