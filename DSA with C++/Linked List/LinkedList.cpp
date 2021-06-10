0e3abad0b1f96f5bbf851cf59ac46dae8100b77f
Lakshay Gola <60484223+lakshaygola@users.noreply.github.com>
2021-06-11 01:58:26 +0530

    Add files via upload

--- DSA with C++/Linked List/LinkedList.cpp
+++ DSA with C++/Linked List/LinkedList.cpp
@@ -6,11 +6,10 @@ using namespace std;
 
 // Class node to define the basic structure of the nodes
 class Node{
-private:
+public: 
 	int data;
 	Node *next;
 
-public:
 	Node(int val){
 		data = val;
 		next = NULL;
@@ -18,12 +17,49 @@ public:
 };
 
 // Function to inerst the node at the tail / end
-void insertToTail(Node* &head, int val){
-	Node* n = new Node(val);
-	
+void insertAtTail(Node* &head, int val){
+	Node* temp = new Node(val);
+
+    if (head == NULL){
+        head = temp;
+        return;
+    }
+
+    Node* p = head;
+    while (p->next != NULL){
+        p = p->next;
+    }
+	p->next= temp;
+}
+
+// Function to add the element to the head of the list
+void insertAthead(Node* &head, int val){
+    Node *temp= new Node(val);
+
+    temp->next = head;
+    head = temp;
+}
+
+// Function to print all the linked list
+void print(Node *head){
+    Node *p= head;
+    while (p != NULL){
+        cout<<p->data<<" ";
+        p= p->next;
+    }
+    cout<<endl;
 }
 
 int main(){
+
+    Node *head= NULL;
+
+    insertAtTail(head, 50);
+    insertAtTail(head, 82);
+    insertAtTail(head, 89);
+
+    insertAthead(head, 60);
+    print(head);
 
 	return 0;
 }
